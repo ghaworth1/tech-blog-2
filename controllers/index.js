cde46a0935878616966af78bf24396d92abac1d8
George Haworth <ghaworth1@gmail.com>
2023-09-13 21:33:18 -0500

    different boilerplate

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-route");
+
+
 // Set up routes
-router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
 // Export the router
 module.exports = router;
